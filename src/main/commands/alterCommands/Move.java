57bf7309473af8db345de4c84ccd4230a5b40cab
BenbounBigHydra <benoit.jaques@sunrise.ch>
2025-05-24 14:51:55 +0200

    Moooove

--- src/main/commands/alterCommands/Move.java
+++ src/main/commands/alterCommands/Move.java
@@ -2,14 +2,37 @@ package main.commands.alterCommands;
 
 import main.Game;
 
-public abstract class Move extends AlterCommand{
-    
+public abstract class Move extends AlterCommand {
+
     public Move(String descriptionTransfer, String actionTransfer) {
         super(descriptionTransfer, actionTransfer);
     }
-    
-    protected void movePlayer(int x, int y){
+
+    @Override
+    public String executeCommand() {
         int[] PP = Game.getInstance().getWorldMap().getPP();
+        int[] futurePP = this.calculatePP(PP);
+        boolean canMove = this.checkMapBorder(futurePP);
+        if (!canMove) {
+            return "You've reach the end of the map.";
+        }
+
+        //Regarde si la case où se déplacer est vérouillée
+        canMove = Game.getInstance().getWorldMap().getMap()[futurePP[0]][futurePP[1]].getUnlockedState();
+
+        if (!canMove) {
+            return "This location is locked";
+        }
+
+        Game.getInstance().getWorldMap().setPP(futurePP[0], futurePP[1]);
+
+        return "You've enter a new Location";
+    }
+
+    protected abstract int[] calculatePP(int[] actualPosition);
+
+    private boolean checkMapBorder(int[] futurePP) { //Check tous les bords de la map 
+        return futurePP[0] <= Game.getInstance().getWorldMap().getMap().length && futurePP[0] >= 0 && futurePP[1] <= Game.getInstance().getWorldMap().getMap()[0].length && futurePP[1] >= 0;
     }
 
 }
