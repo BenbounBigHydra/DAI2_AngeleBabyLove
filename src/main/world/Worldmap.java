f98b6d8d3de6b56001b6653ceaacadfe2eb69212
Pioupiou <etienne.bergeon@heig-vd.ch>
2025-05-26 09:57:40 +0200

    etienne merdouille

--- src/main/world/Worldmap.java
+++ src/main/world/Worldmap.java
@@ -3,6 +3,8 @@ package main.world;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
+
 public class Worldmap {
 
     private int[] playerPosition;
@@ -22,14 +24,14 @@ public class Worldmap {
         map[0][2].unlock();
         map[1][0] = new Location("Garden", "The house's garden", null, null);
         map[1][0].unlock();
-        map[1][1] = new Location("House", "The house", null, new Key("The house's key"));
-        map[1][2] = new Location("Garden", "The house's garden", null, null);
+        map[1][1] = new Location("House", "The house", null, null);
+        map[1][2] = new Location("Cafeteria", "The Gertrude's Cafeteria", null, new Key("The Cafeteria's key");
         map[1][2].unlock();
-        map[2][0] = new Location("Forest", "The forest next to the house", null, null);
+        map[2][0] = new Location("School", "The building lobby", null, null);
         map[2][0].unlock();
-        map[2][1] = new Location("Forest", "The forest next to the house", null, null);
+        map[2][1] = new Location("Court", "The concrete courtyard", null, null);
         map[2][1].unlock();
-        map[2][2] = new Location("Cottage", "The cottage at the bottom of the garden", null, null);
+        map[2][2] = new Location("Court", "The concrete courtyard", null, null);
         map[2][2].unlock();
         
         List<Item> toAdd = new ArrayList<>();
