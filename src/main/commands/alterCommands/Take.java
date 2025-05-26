b65eb40e533c1e029383960e30650f97d26838ab
BenbounBigHydra <benoit.jaques@sunrise.ch>
2025-05-26 10:47:38 +0200

    Update Take.java

--- src/main/commands/alterCommands/Take.java
+++ src/main/commands/alterCommands/Take.java
@@ -1,14 +1,28 @@
 package main.commands.alterCommands;
 
-public class Take extends AlterCommand{
-    
+import main.Game;
+import main.world.Item;
+import main.world.Location;
+
+public class Take extends AlterCommand {
+
     public Take(String descriptionTransfer, String actionTransfer) {
         super(descriptionTransfer, actionTransfer);
     }
 
     @Override
     public String execute() {
-        return "pas encore codé";
+        Location currentLoc = Game.getInstance().getWorldMap().getMap()[Game.getInstance().getWorldMap().getPP()[0]][Game.getInstance().getWorldMap().getPP()[1]];
+        if (currentLoc.getListItems().isEmpty()) {
+            return "There is no item to take";
+        } else {
+            int n = 1;
+            for (Item i : currentLoc.getListItems()) {
+                System.out.println("Item " + n + " : " + i.getDescritpion());
+            }
+            do {
+                System.out.println("Witch item do you want to take (enter the number) ? ");
+            } while (false);
+        }
     }
-    
 }
