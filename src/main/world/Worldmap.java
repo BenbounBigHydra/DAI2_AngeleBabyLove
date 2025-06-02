2f7ed2415a2a162b251c07b9570ee6b7debd6cd3
Chlopinette <chloe.muehlemann@outlook.com>
2025-06-02 11:10:19 +0200

    New locations + descr

--- src/main/world/Worldmap.java
+++ src/main/world/Worldmap.java
@@ -10,7 +10,7 @@ public class Worldmap {
 
     public Worldmap() {
         //Crée la Map
-        this.map = new Location[3][3];
+        this.map = new Location[4][3];
 
         //Crée les item pour les Locations
         //Créer les clés
@@ -23,17 +23,17 @@ public class Worldmap {
         //Créer le puzzle 2 et ajouter les clés
         reward.clear();
         reward.add(keyCafet);
-        Puzzle puzzleOfSteveHour = new Puzzle("seven", "Oh thank you for your help! I was going to be late for my non-existent class. For your information, I saw Léo leaving a letter for Jerem in the cafeteria. You should pick it up before he finds it. ", reward);
+        Puzzle puzzleOfSteveHour = new Puzzle("seven", "Oh thank you for your help! I was going to be late for my non-existent class. For your information, I saw Léo leaving a letter for Jerem in the cafeteria. You should pick it up before he finds it. The cafeteria is closed, but here's the access badge. You should take it with you. ", reward);
         //Créer les lettres
-        Letter letterSteve = new Letter("A letter smelling fish", "Hello there ! I have a lesson soon but I have no idea wshat time it is. Could you tell me, I lost my phone and I'm currently working on...well nothing but...you see");
-        Letter letterLeo = new Letter("A well folded letter", "Hey Jerem ! I left the key of the chill hidden in our favourite machine of the cafeteria. You can grab it next time.");
+        Letter letterSteve = new Letter("A letter smelling fish", "Hello there! I have an appointment soon, but I have no idea what time it is because I lost my phone again. Can you help me out? I'm currently working on... well, not much, but I'm pretending to. Thanks for your help! -Steve");
+        Letter letterLeo = new Letter("A well folded letter", "Hey Jerem ! I left the key of the chill hidden in our favourite machine of the cafeteria. You can grab it next time. Has anyone seen my dog? She stole my magic teleportation crystal again... -Léo");
 
         //Crée et insère les Location dans la map
         //(pour le moment elles sont normalisées pour tester le programme)
-        map[0][0] = new Location("School", "The working place. Steve is sit in a corner doing anything but work.", puzzleOfSteveHour, null);
+        map[0][0] = new Location("School", "The place to study. Steve is sitting in a corner. He is pretending to work very well.", puzzleOfSteveHour, null);
         map[0][0].unlock();
-        map[0][1] = new Location("Chill", "The best place on earth", null, keyChill);
-        map[0][2] = new Location("Cafeteria", "The place to eat sweets and drink lots of coffee", puzzleOfHiddenKey, null);
+        map[0][1] = new Location("Chill", "The best place on earth <3", null, keyChill);
+        map[0][2] = new Location("Cafeteria", "The place to eat sweets and drink lots of coffee.", puzzleOfHiddenKey, null);
         map[0][2].unlock();
         map[1][0] = new Location("School", "The building lobby", null, null);
         map[1][0].unlock();
@@ -48,6 +48,13 @@ public class Worldmap {
         map[2][2] = new Location("Court", "The concrete courtyard", null, null);
         map[2][2].unlock();
         map[2][2].addItems(letterSteve);
+        map[3][0] = new Location("School", "The building lobby", null, null);
+        map[3][0].unlock();
+        map[3][1] = new Location("Court", "The courtyard where Nami is sitting. She greets you with her paw.", null, null);
+        map[3][1].unlock();
+        map[3][2] = new Location("Baleinev room", "The Baleinev room. Raph is sitting on the sofa. You can hear him explaining to a new student that the fountain in the courtyard is a sundial.", null, null);
+        map[3][2].unlock();
+
 
         //Défini la position du player
         playerPosition = new int[2];
